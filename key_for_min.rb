16ba6f69327375b76e0a3e33e6e3730130fc1b7f
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-26 23:53:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   name_hash.each do |key, value|
-    if lowest_value = nil || value < lowest_value
+    if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = key
     end
